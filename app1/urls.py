08578cf36d8c609ca15d3dcb219075ea69e3e869
Parvathykrishna1 <parvathyk285@gmail.com>
2024-04-18 11:53:58 +0530

    first commit

--- app1/urls.py
+++ app1/urls.py
@@ -3,10 +3,8 @@ from .views import *
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    path('login/',Login.as_view(),name='login'),
     path('userregister/',UserRegister.as_view(),name='userregister'),
     path('userlist/',UserList.as_view(),name='userlist'),
-    path('userbyid/<int:pk>/',UserById.as_view(),name='userbyid'),
     path('refreshtoken/',jwt_views.TokenRefreshView.as_view(),name ='refreshtoken'),
     path('access_token/',jwt_views.TokenObtainPairView.as_view(),name ='access token'),
 ]
