4af14a9d60bcbd4482d5ab6019ee87fc8a7360cd
Parvathy krishna <95429804+Parvathykrishna1@users.noreply.github.com>
2024-04-19 08:45:22 +0530

    commit

--- app1/urls.py
+++ app1/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('login/',Login.as_view(),name='login'),
     path('userregister/',UserRegister.as_view(),name='userregister'),
     path('userlist/',UserList.as_view(),name='userlist'),
-    path('userbyid/<int:pk>/',UserById.as_view(),name='userbyid'),
     path('refreshtoken/',jwt_views.TokenRefreshView.as_view(),name ='refreshtoken'),
     path('access_token/',jwt_views.TokenObtainPairView.as_view(),name ='access token'),
 ]
